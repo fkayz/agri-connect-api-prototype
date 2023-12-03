bd89abf2deb7af8427d528190f00c3bef33b2061
hbfl3x <hbfl3x@penguin>
2023-12-03 10:40:45 +0200

    add create a new product in marketplace functionality

--- client/src/screens/AddProduct.jsx
+++ client/src/screens/AddProduct.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 import axios from 'axios'
 import Navbar from '../components/Navbar'
 import SideNavBar from '../components/SideNavBar'
 import getCookie from '../hooks/getCookie'
-import productImg from '../assets/post-user-1.jpeg'
+import addProductImg from '../assets/add_product.png'
 
 
 const AddProduct = () => {
@@ -12,14 +13,15 @@ const AddProduct = () => {
     const [ productName, setProductName ] = useState('')
     const [ productDescription, setProductDescription ] = useState('')
     const [ productPrice, setProductPrice ] = useState('')
+    const [ productImgLink, setProductLink ] = useState('')
+    const [ productImg, setProductImg ] = useState('')
+    const [ productImgUploadPrg, setProductImgUploadPrg ] = useState(0)
 
     const [ currentUser, setCurrentUser ] = useState(JSON.parse(getCookie('currentUser')))
 
     const navigate = useNavigate()
 
-
-    const createProductHandle = async (event) => {
-        event.preventDefault()
+    const saveChanges = async () => {
         try{
             const productData = {
                 product_owner_id: currentUser.id,
@@ -27,7 +29,7 @@ const AddProduct = () => {
                 name: productName,
                 description: productDescription,
                 price: productPrice,
-                productImage: 'https://img.freepik.com/free-vector/organic-flat-farming-profession-illustration_23-2148897363.jpg?w=1380&t=st=1700387349~exp=1700387949~hmac=a0114fe47105d5f0c64fab56568ef71675fab65fdf07179cb112ede6cda3e731'
+                productImage: productImgLink
             }
             const product = await axios.post('http://127.0.0.1:8000/api/products/add', productData)
 
@@ -36,8 +38,49 @@ const AddProduct = () => {
             }
         }catch(err){
             alert('error creating product, try again')
-            console.log('erro  creating product', err)
+            console.log('error  creating product', err)
         }
+    }
+
+    const uploadProductImageToFirebase = (img) => {
+        const storageRef = ref('/uploaded_files/product/image/', img.name)
+        const uploadTask = uploadBytesResumable(storageRef, img)
+
+        uploadTask.on('state_changed', 
+            (snapshot) => {
+                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+                setProductImgUploadPrg(progress)
+            },
+            (err) => { return alert('failed to upload img. try again', err) },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref)
+                .then(async (url) => {
+                    setProductLink(url);
+                    await saveChanges()
+                })
+            }
+        )
+    }
+
+    const saveToDB = async (event) => {
+        event.preventDefault()
+        await uploadProductImageToFirebase(productImg)
+    }
+
+    const previewProductImage = (img) => {
+        const reader = new FileReader()
+        const imgPreview = document.querySelector('#imgPreview')
+
+        reader.onload = (e) => {
+            imgPreview.src = e.target.result
+            setProductImg(img.files[0])
+        }
+
+        reader.readAsDataURL(img.files[0])
+    }
+
+    const uploadProductImage = (e) => {
+        previewProductImage(e.target)
     }
 
     return (
@@ -56,23 +99,29 @@ const AddProduct = () => {
                         <div className='row mt-5'>
                             <div className='col-md-3'>
                                 <div>
-                                    <img src={productImg} width='100%' className='rounded' alt='product image' />
+                                    <img src={addProductImg} id='imgPreview' width='100%' className='rounded' alt='product image' />
+                                    {/* <h3 contentEditable id='productNameEditable'>Enter product name</h3> */}
                                 </div>
                             </div>
 
                             <div className='col-md-9'>
-                                <div className='bg-white p-4 rounded px-4'>
-                                    <form onSubmit={createProductHandle}>
-                                        <div className='mb-3'>
-                                            <input value={productName} onChange={(e) => setProductName(e.target.value)} type='text' placeholder='Enter product name' className='form-control' />
+                                <div className='p-4 rounded px-4'>
+                                    <form onSubmit={saveToDB}>
+                                        <div className='mb-2'>
+                                            <input value={productName} onChange={(e) => setProductName(e.target.value)} type='text' placeholder='Enter product name' className='add-product-inpt form-control' />
+                                        </div>
+
+                                        <div className='mb-2'>
+                                            <textarea  value={productDescription} onChange={(e) => setProductDescription(e.target.value)} className='add-product-inpt form-control' placeholder='Enter product description'></textarea>
                                         </div>
 
                                         <div className='mb-3'>
-                                            <textarea  value={productDescription} onChange={(e) => setProductDescription(e.target.value)} className='form-control' placeholder='Enter product description'></textarea>
+                                            <input  value={productPrice} onChange={(e) => setProductPrice(e.target.value)} type='number' placeholder='Enter product price' className='add-product-inpt form-control' />
                                         </div>
 
                                         <div className='mb-3'>
-                                            <input  value={productPrice} onChange={(e) => setProductPrice(e.target.value)} type='number' placeholder='Enter product price' className='form-control' />
+                                            <label htmlFor="productImg">Upload product image</label>
+                                            <input type='file' id='productImg' onChange={uploadProductImage} className='add-product-inpt form-control' />
                                         </div>
 
                                         <div className='mb-3'>
